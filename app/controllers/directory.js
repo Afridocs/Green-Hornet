c597bee61914611ce76cb3fecde0ad866a1b90ab
j-norwood-young <jason@10layer.com>
2013-10-04 10:53:51 +0200

    Directory service

---
+++ app/controllers/directory.js
@@ -0,0 +1,20 @@
+var mongoose = require("mongoose");
+var Recipient = require("../models/recipient");
+
+mongoose.connect('mongodb://localhost/greenhornet');
+
+var db = mongoose.connection;
+var connected = false;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+	connected = true;
+});
+
+module.exports = {
+
+	get: function(req, res, next) {
+		Recipient.Model.find({ }, function(err, recipients) {
+			res.send(recipients);
+		});
+	}
+}
--- app/server.js
+++ app/server.js
@@ -5,18 +5,23 @@ var restify = require('restify');
 var public_key = require('./controllers/public_key.js');
 var message = require('./controllers/message.js');
 var recipient = require('./controllers/recipient.js');
+var directory = require('./controllers/directory.js');
 
 // Set up server
 var server = restify.createServer();
 server.use(restify.bodyParser());
 
-//Routes
+// PPK Routes
 server.post("/api/public_key", public_key.post);
 server.head("/api/public_key", public_key.post);
-server.post("/api/message", message.post);
-
 server.get("/recipient/public_key", recipient.public_key);
+
+// Messaging Routes
+server.post("/api/message", message.post);
 server.post("/recipient/message", recipient.message);
+
+// Directory Service Routes
+server.get("/api/directory", directory.get);
 
 // Start server
 server.listen(3000, function() {
